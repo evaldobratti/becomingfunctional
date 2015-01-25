193a388548f661f0b97e452d9f8ca14d16d8f6e2
Evaldo Bratti <evaldo.bratti@gmail.com>
2015-01-25 18:17:44 -0200

    chapter 2 ended

--- fp-in-scala/Chapter2.scala
+++ fp-in-scala/Chapter2.scala
@@ -12,4 +12,16 @@ object Chapter2 {
 
     go(0, 0, 1)
   }
+
+  def isSorted[A](array: Array[A], ordered: (A, A) => Boolean): Boolean =
+    if (array.size < 2)
+      true
+    else
+      ordered(array(0), array(1)) && isSorted(array.drop(1), ordered)
+
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = a => b => f(a, b)
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C = (a, b) => f(a)(b)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C = a => f(g(a))
 }
